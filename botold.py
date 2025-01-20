2f047f1d85ce9046a00f0838d395ca4de952e20d
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 02:29:10 +0300

    Add files via upload

--- botold.py
+++ botold.py
@@ -1,21 +1,25 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Application, CommandHandler
 
 TOKEN = '7266775254:AAEvHJqDPcE1IzSnvDigrjZ8IUY3vdWbjyA'
 
-def start(update, context):
+async def start(update, context):
     keyboard = [
         [InlineKeyboardButton("Запустить игру", url="https://dontevil.github.io/dontevil1.github.io/")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Добро пожаловать в GTA 5 Bot!", reply_markup=reply_markup)
+    await update.message.reply_text("Добро пожаловать в GTA 5 Bot!", reply_markup=reply_markup)
 
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+async def main():
+    # Создаем объект приложения (bot)
+    application = Application.builder().token(TOKEN).build()
+
+    # Регистрация обработчиков
+    application.add_handler(CommandHandler("start", start))
+
+    # Запуск бота
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
