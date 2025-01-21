0d639af37f7794be7dcfbbc23feec2bda8c2a9a1
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 03:02:07 +0300

    Add files via upload

--- botold.py
+++ botold.py
@@ -5,24 +5,22 @@ from telegram.ext import Application, CommandHandler
 async def start(update, context):
     keyboard = [
         [
-            InlineKeyboardButton("Open Web App", web_app=WebAppInfo(url="https://your-web-app-url.com"))
+            InlineKeyboardButton("Open Web App", web_app=WebAppInfo(url="https://dontevil.github.io/dontevil1.github.io/"))
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Hello! Click the button below to open the Web App:", reply_markup=reply_markup)
 
 # Главная функция
-async def main():
+def main():
+    # Создаем объект приложения и передаем токен
     application = Application.builder().token("7266775254:AAEvHJqDPcE1IzSnvDigrjZ8IUY3vdWbjyA").build()
 
     # Добавляем обработчик команды /start
     application.add_handler(CommandHandler("start", start))
 
-    # Запускаем бота
-    await application.run_polling()
+    # Запускаем бота с использованием встроенной асинхронности
+    application.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
-    # Вместо asyncio.run используем event loop из telegram.ext
-    asyncio.create_task(main())
-    asyncio.get_event_loop().run_forever()
+    main()
