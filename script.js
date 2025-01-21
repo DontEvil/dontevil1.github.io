42abd9109f9ef00557757a61aeb99eeb12902523
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 06:31:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ document.getElementById('montage-btn').addEventListener('click', function() {
     document.getElementById('modal-montage').style.display = 'flex';
 });
 
-// Функция для закрытия модальных окон
+// Функция для закрытия модальных окон с анимацией
 document.querySelectorAll('.modal').forEach(modal => {
     modal.addEventListener('click', function(event) {
         if (event.target === modal) {
-            modal.style.display = 'none';
+            modal.classList.add('hide');
+            setTimeout(() => {
+                modal.style.display = 'none';
+                modal.classList.remove('hide');
+            }, 300); // Задержка для завершения анимации
         }
     });
 });
