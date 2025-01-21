1f4a974f1ce8ac3fd8de31ed525166144a7f23b1
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 06:21:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Звук при нажатии на кнопки
-    const buttonClickSound = new Audio('click-sound.mp3');  // Убедитесь, что путь к звуковому файлу верен
+// Функции для открытия модальных окон
+document.getElementById('store-btn').addEventListener('click', function() {
+    document.getElementById('modal-store').style.display = 'flex';
+});
 
-    const buttons = document.querySelectorAll('.button');
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            buttonClickSound.play();  // Воспроизводим звук нажатия
-        });
-    });
+document.getElementById('mfg-btn').addEventListener('click', function() {
+    document.getElementById('modal-mfg').style.display = 'flex';
+});
 
-    // Функции для кнопок
-    document.getElementById('storeBtn').addEventListener('click', function() {
-        window.open('https://example.com/store', '_blank');  // Открываем ссылку магазина
-    });
+document.getElementById('montage-btn').addEventListener('click', function() {
+    document.getElementById('modal-montage').style.display = 'flex';
+});
 
-    document.getElementById('mfgBtn').addEventListener('click', function() {
-        window.open('https://drive.google.com/file/d/1uLDt0s_8TNg1xtuqR3KP8w4xSIgbLmvy/view', '_blank');  // Открываем ссылку для MFG 1.0
-    });
-
-    document.getElementById('montageBtn').addEventListener('click', function() {
-        window.open('https://drive.google.com/file/d/1lLObk3qjSrsEjtbfXVAJIYrX7YdY-jrw/view?usp=sharing', '_blank');  // Открываем ссылку для Монтажа
+// Функция для закрытия модальных окон
+document.querySelectorAll('.modal').forEach(modal => {
+    modal.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
     });
 });
