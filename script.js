11f1556cf66e5f9d8b8791ed8b5544ab1f1522c2
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 06:46:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Функции для открытия модальных окон
 document.getElementById('store-btn').addEventListener('click', function() {
     document.getElementById('modal-store').style.display = 'flex';
+    document.getElementById('click-sound').play();
 });
 
 document.getElementById('mfg-btn').addEventListener('click', function() {
     document.getElementById('modal-mfg').style.display = 'flex';
+    document.getElementById('click-sound').play();
 });
 
 document.getElementById('montage-btn').addEventListener('click', function() {
     document.getElementById('modal-montage').style.display = 'flex';
+    document.getElementById('click-sound').play();
 });
 
 // Функция для закрытия модальных окон
@@ -20,13 +23,23 @@ document.querySelectorAll('.modal').forEach(modal => {
     });
 });
 
+// Переключение между ночным и дневным режимами
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('night');
+    document.querySelectorAll('.button').forEach(button => {
+        button.classList.toggle('night');
+    });
+});
+
 // Функция для вращения картинки на 720 градусов
 document.getElementById('character-image').addEventListener('click', function() {
     this.style.transform = 'rotate(720deg)';
     this.style.transition = 'transform 0.5s ease-in-out';
     
-    // Возвращаем начальное положение картинки через небольшой промежуток времени
     setTimeout(() => {
         this.style.transform = 'rotate(0deg)';
     }, 500); // 500ms - время анимации
 });
+
+// Инициализация AOS для анимаций при прокрутке
+AOS.init();
