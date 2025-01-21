cb979419091e6491aa62566b9c71d683fd4932fd
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 06:41:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,15 +11,22 @@ document.getElementById('montage-btn').addEventListener('click', function() {
     document.getElementById('modal-montage').style.display = 'flex';
 });
 
-// Функция для закрытия модальных окон с анимацией
+// Функция для закрытия модальных окон
 document.querySelectorAll('.modal').forEach(modal => {
     modal.addEventListener('click', function(event) {
         if (event.target === modal) {
-            modal.classList.add('hide');
-            setTimeout(() => {
-                modal.style.display = 'none';
-                modal.classList.remove('hide');
-            }, 300); // Задержка для завершения анимации
+            modal.style.display = 'none';
         }
     });
+});
+
+// Функция для вращения картинки на 720 градусов
+document.getElementById('character-image').addEventListener('click', function() {
+    this.style.transform = 'rotate(720deg)';
+    this.style.transition = 'transform 0.5s ease-in-out';
+    
+    // Возвращаем начальное положение картинки через небольшой промежуток времени
+    setTimeout(() => {
+        this.style.transform = 'rotate(0deg)';
+    }, 500); // 500ms - время анимации
 });
