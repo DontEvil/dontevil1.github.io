d6cd931cf363f4dc98106c1410a4c736b42660d1
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 06:07:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Функции для кнопок
-    document.getElementById('redux').addEventListener('click', function() {
-        alert("Редукс нажата!");
+    const hoverSound = document.getElementById('hoverSound');
+    const clickSound = document.getElementById('clickSound');
+
+    // Получаем все кнопки с классом .button
+    const buttons = document.querySelectorAll('.button');
+
+    // Звук при наведении на кнопку
+    buttons.forEach(button => {
+        button.addEventListener('mouseenter', function() {
+            hoverSound.play();  // Проигрываем звук при наведении
+        });
+
+        // Звук при клике на кнопку
+        button.addEventListener('click', function() {
+            clickSound.play();  // Проигрываем звук при клике
+        });
     });
 
-    document.getElementById('montage').addEventListener('click', function() {
-        alert("Монтаж нажата!");
+    // Функционал для кнопок
+    document.getElementById('mfgBtn').addEventListener('click', function() {
+        window.open('https://drive.google.com/file/d/1uLDt0s_8TNg1xtuqR3KP8w4xSIgbLmvy/view', '_blank');
     });
 
-    document.getElementById('preview').addEventListener('click', function() {
-        alert("Превью нажата!");
+    document.getElementById('montageBtn').addEventListener('click', function() {
+        window.open('https://drive.google.com/file/d/1lLObk3qjSrsEjtbfXVAJIYrX7YdY-jrw/view?usp=sharing', '_blank');
+    });
+
+    // Здесь можно добавить функциональность для Магазина
+    document.getElementById('storeBtn').addEventListener('click', function() {
+        alert('Магазин скоро будет доступен!');
     });
 });
