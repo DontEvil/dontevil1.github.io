1539750a2fcc6d5c318487b63ade0cac6afa5af6
DontEvil <143675181+DontEvil@users.noreply.github.com>
2025-01-21 04:03:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Устанавливаем имя пользователя и функции кнопок
-    const userName = "Имя пользователя";  // Замените на данные из API бота
-    document.getElementById('username').textContent = userName;
-
     // Функции для кнопок
-    document.getElementById('redux-btn').addEventListener('click', function() {
-        // Действие для Редукс
-        console.log("Редукс нажата");
+    document.getElementById('redux').addEventListener('click', function() {
+        alert("Редукс нажата!");
     });
 
-    document.getElementById('montage-btn').addEventListener('click', function() {
-        // Действие для Монтаж
-        console.log("Монтаж нажата");
+    document.getElementById('montage').addEventListener('click', function() {
+        alert("Монтаж нажата!");
     });
 
-    document.getElementById('preview-btn').addEventListener('click', function() {
-        // Действие для Превью
-        console.log("Превью нажата");
+    document.getElementById('preview').addEventListener('click', function() {
+        alert("Превью нажата!");
     });
 });
